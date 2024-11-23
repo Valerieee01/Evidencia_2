e7d1c6a0bf9320600ea626415eae3e4a1b854c01
Valerieee01 <zharmel.afa@gmail.com>
2024-11-22 21:27:27 -0500

    terminado

--- app.js
+++ app.js
@@ -59,3 +59,53 @@ const leerTodos = async () => {
     }
 };
 
+// definimos una funcion llamada crearObjeto la cual nos siver para crear el objeto donde se almacenara todos los arrays
+const crearObjeto = (usuariosPost, usuariosComent, usuariosAlbum, usuariosPhoto, usuariosTodo) => {
+    
+    const datosUsuario = {
+        "posts": usuariosPost,
+        "comments": usuariosComent,
+        "albums": usuariosAlbum,
+        "photos": usuariosPhoto,
+        "todos": usuariosTodo,
+    };
+
+    console.log(datosUsuario);
+};
+// definimos una funcion llamda filtrarInfo donde vamos a filtrar toda la informacion de JSON para guardarla en el objeto
+const filtarInfo = async () => {
+        const usuarios = await leerUser();
+        const Posts = await leerPost();
+        const Comments = await leerComments();
+        const Albums = await leerAlbums();
+        const Photos = await leerPhotos();
+        const Todos = await leerTodos();
+        
+    // creamos un forEach por cada user vamos a obtener la informacion
+    usuarios.forEach(user => {
+        let usuariosPost = Posts.filter(post => post.userId === user.id);
+        let usuariosComent = Comments.filter(comment => 
+            usuariosPost.some(post => post.id === comment.postId)
+        );
+        let usuariosAlbum = Albums.filter(album => user.id === album.userId);
+        let usuariosPhoto = Photos.filter(photo => usuariosAlbum.some(album => album.id === photo.albumId));
+        let usuariosTodo = Todos.filter(todo => todo.userId === user.id);
+
+        //mostramos el id del usuario 
+        console.log(`Array del usuario ${user.id}`);
+        // llamamos a la funcion crearObjeto para que cree el objeto en cada vuelta del foreach 
+        crearObjeto(usuariosPost, usuariosComent, usuariosAlbum, usuariosPhoto, usuariosTodo)
+
+    });
+}
+
+
+const mostrarArrays = async () => {
+    try {
+        filtarInfo()
+    } catch (error) {
+        console.error("No se pudo cargar cargar correctamente el JSON");
+    }
+}
+
+mostrarArrays(); 
